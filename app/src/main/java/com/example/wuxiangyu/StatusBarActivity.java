bbd348f4a5d0de40ce3a5330a251ea37954bf85d
wuxiangyu <wuxaingyu.lc@bytedance.com>
2018-09-26 10:37:01 +0800

    feat：沉浸式使状态栏为透明，然后设置padding 比较合适。

---
+++ app/src/main/java/com/example/wuxiangyu/StatusBarActivity.java
@@ -0,0 +1,34 @@
+package com.example.wuxiangyu;
+
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.WindowManager;
+
+import com.example.wuxiangyu.first.R;
+
+
+public class StatusBarActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            int option = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+            getWindow().getDecorView().setSystemUiVisibility(option);
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        }
+        setContentView(R.layout.activity_statusbar);
+        getWindow().getDecorView().setPadding(0, getStatusBarHeight(), 0, 0);
+
+    }
+
+    private int getStatusBarHeight() {
+        int resourceId = getResources().getIdentifier("status_bar_height", "dimen", "android");
+        return getResources().getDimensionPixelSize(resourceId);
+    }
+}
