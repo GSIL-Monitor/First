a0fb7bc2d01f7307492ab0690fbf269f19168035
wuxiangyu <wuxaingyu.lc@bytedance.com>
2018-09-29 16:16:19 +0800

    feat：fragment通过navigation跳转到其他activity里面，也就是跳出了该fragment对应的navigation了

---
+++ app/src/main/java/com/example/wuxiangyu/navisecond/NavigationsSecondActivity.kt
@@ -0,0 +1,12 @@
+package com.example.wuxiangyu.navisecond
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.example.wuxiangyu.first.R
+
+class NavigationsSecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_navigations_second)
+    }
+}
--- app/src/main/java/com/example/wuxiangyu/navisecond/first/NavigationsFirstSubAFragment.kt
+++ app/src/main/java/com/example/wuxiangyu/navisecond/first/NavigationsFirstSubAFragment.kt
@@ -4,12 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import com.example.wuxiangyu.first.R
 import com.example.wuxiangyu.navisecond.BaseFragment
 
 class NavigationsFirstSubAFragment : BaseFragment() {
     lateinit var tvFirst: TextView
+    lateinit var btnSecondNavigations: Button
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_navigations_first_suba, container, false)
         initView(view)
@@ -19,5 +21,9 @@ class NavigationsFirstSubAFragment : BaseFragment() {
 
     private fun initView(view: View) {
         tvFirst = view.findViewById(R.id.tvFirst)
+        btnSecondNavigations = view.findViewById(R.id.btnSecondNavigations)
+        btnSecondNavigations.setOnClickListener{
+            navController?.navigate(R.id.action_navigationsFirstSubAFragment_to_navigationsSecondActivity)
+        }
     }
 }
